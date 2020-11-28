1c67e0a30931cf999d04f9c14202111378509ca2
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:43:12 -0300

    Add files via upload

---
+++ EX-10-LISTA-1-Yohan_Aquino/EX-10-LISTA-1-Yohan_Aquino/IMC.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_10_LISTA_1_Yohan_Aquino
+{
+    class IMC
+    {
+        private double p;
+        private double h;
+        private double r;
+
+        public void setPeso(double x) 
+        {
+            p = x;
+        }
+
+        public void setAltura(double x)
+        {
+            h = x;
+        }
+
+        public void calcularRelação() 
+        {
+            r = p /Math.Pow(h, 2);
+        }
+        public string getRelação()
+        {
+            if (r < 20)
+            {
+                return "abaixo do peso";
+            }
+            else 
+            {
+                if (r >= 25)
+                {
+                    return "acima do peso";
+                }
+                else 
+                {
+                     return "no peso ideal";
+                }
+            }
+        }
+
+    }
+}
---
+++ EX-10-LISTA-1-Yohan_Aquino/EX-10-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_10_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*11. Entrar com o peso e a altura de uma determinada pessoa. Após a digitação, exibir se esta
+pessoa está ou não com seu peso ideal. Veja tabela da relação peso/altura².
+Relação peso/altura² (R) Mensagem
+R < 20 Abaixo do peso
+20 <= R < 25 Peso ideal
+R >= 25 Acima do peso*/
+            IMC i = new IMC();
+
+            Console.WriteLine("Digete o peso da pessoa:");
+            i.setPeso(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite a altura da pessoa");
+            i.setAltura(double.Parse(Console.ReadLine()));
+            i.calcularRelação();
+            Console.WriteLine("o IMC da pessoa mostra que está {0}",i.getRelação());
+            Console.ReadKey();
+        }
+    }
+}
